d529d0aa0abd2c1d226bb32dd4fc49ccc95a6258
NickWattsCS <nsw12b@my.fsu.edu>
2019-10-05 00:13:58 -0400

    Added serializers, models, and started adding views
    Models are incomplete, as Admin has not been properly interfaced

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+"""This acts as an abstract class to contain common data"""
+class User(models.Model):
+	first_name = models.CharField(max_length=50, default='')
+	last_name = models.CharField(max_length=50, default='')
+	joined = models.DateTimeField(auto_now_add=True)
+	team = models.CharField(max_length=100,default='')
+	class Meta:
+		abstract = True
+		ordering = ['joined']
+
+class Admin(User):
+
+class Staff(User):
+	department = models.CharField(max_length=50,default='')
+	points = models.IntegerField(max_length=10,blank=True,default=0)
+	mon_raised = models.IntegerField(max_length=20,blank=True,default=0)
+
+class Dancer(User):
+	points = models.IntegerField(max_length=10,blank=True,default=0)
+	mon_raised = models.IntegerField(max_length=20, blank=True,default=0)
+
+class Guest(User):
+	first_name = models.CharField(null=True)
+	last_name = models.CharField(null=True)
+	joined = models.DateField(null=True)
+	team = models.CharField(null=True)
+
