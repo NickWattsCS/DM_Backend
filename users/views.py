149e61a743edac218247df08981885ff038afdd0
NickWattsCS <nsw12b@my.fsu.edu>
2019-10-10 20:22:15 -0400

    Changed previous functions to viewsets
    Includes UserViewSets, AdminViewSets, StaffViewSets, DancerViewSets, GuestViewSets

--- users/views.py
+++ users/views.py
@@ -1,45 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import viewsets
 from users.models import *
 from users.serializers import *
 # Create your views here
 
-@crsf_exempt
-def user_list(request):
-	if request.method == "GET":
-		user_l = User.objects.all()
-		serializer = UserSerializer(user_l, many=True)
-		return JsonResponse(serializer.data, safe=False)
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all().order_by("last_name")
+	serializer_class = UserSerializer
 
-	elif request.method == "POST":
-		data = JSONParser().parse(request)
-		serializer = UserSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
+class DancerViewSet(viewsets.ModelViewSet):
+	queryset = Dancer.objects.all().order_by("last_name")
+	serializer_class = DancerSerializer
 
-@crsf_exempt
-def user_detail(request, pk):
-	try:
-		user = User.objects.get(pk = pk)
-	except User.DoesNotExist:
-		return HttpResponse(status = 404)
+class StaffViewSet(viewsets.ModelViewSet):
+	queryset = Staff.objects.all().order_by("last_name")
+	serializer_class = StaffSerializer
 
-	if request.method == "GET":
-		serializer = UserSerializer(user)
-		return JsonResponse(serializer.data)
+class AdminViewSet(viewsets.ModelViewSet):
+	queryset = Admin.objects.all().order_by("last_name")
+	serializer_class = AdminSerializer
 
-	elif request.method == "PUT":
-		data = JSONParser().parse(request)
-		serializer = UserSerializer(user, data = data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse(serializers.data)
-		return JsonResponse(serializer.errors, status = 400)
-	elif request.method == "DELETE":
-		user.delete()
-		return HttpResponse(status = 204)
-
+class GuestViewSet(viewsets.ModelViewSet):
+	queryset = Guest.objects.all().order_by("joined")
+	serializer_class = GuestSerializer
