398d1603756a4a5e1bfd71eee51505aca9557797
ces17g <54723379+ces17g@users.noreply.github.com>
2019-10-20 16:52:59 -0400

    Group Models that I had in testing : Admin, Staff, and User
    These are the models I had in testing before switching gears and learning about the graphing options for the app.

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import Group
 from django.shortcuts import render
+from groups.models import *
+from groups.serializers import *
 
-# Create your views here.
+from rest_framework import generics, permissions, viewsets
+from snippets.permissions import IsOwnerOrReadOnly
+
+class AdminGroupList(generics.ListAPIView):
+    queryset = AdminGroup.objects.all().order_by("lastName")
+    serializer_class = GroupSerializer
+
+class StaffGroupDetail(generics.RetrieveAPIView):
+    queryset = StaffGroup.objects.all().order_by("lastName")
+    serializer_class = GroupSerializer
+
+class UserGroupDetail(generics.RetrieveAPIView):
+    queryset = UserGroup.objects.all().order_by("lastName")
+    serializer_class = GroupSerializer
