7be5d33f120e10b3400705a4e3fbf24487c5ffb3
ces17g <54723379+ces17g@users.noreply.github.com>
2019-11-07 20:19:54 -0500

    Fields fixed
    Id and name

--- groups/serializers.py
+++ groups/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['name', 'mon_raised', 'points']
-        read_only_fields = ('id','mon_raised', 'points')
+        fields = ['id', 'name', 'mon_raised', 'points']
+        read_only_fields = ('id', 'name', 'mon_raised', 'points')
