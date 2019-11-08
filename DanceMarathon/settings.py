c2ade356adca9d6ae43efe0a263d70174cf00c42
ces17g <54723379+ces17g@users.noreply.github.com>
2019-11-07 19:33:50 -0500

    Includes groups
    Urls.py:
    router.register for groups and
    urlpatterns for groups
    
    Settings.py:
    installed_apps has GroupsConfig

--- DanceMarathon/settings.py
+++ DanceMarathon/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
     'users.apps.UsersConfig',
+    'groups.apps.GroupsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
