184a92f80a613df7b0a8bd2f43ea190b96874c5b
NickWattsCS <nsw12b@my.fsu.edu>
2019-11-13 15:50:54 -0500

    Removed child classes of DanceUser

--- users/models.py
+++ users/models.py
@@ -1,12 +1,12 @@
 import datetime
 from django.db import models
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 
 """
-DanceUser is the abstract class from which its children inherit the data members:
+DanceUser is the class from which its children inherit the data members:
 team: which uses Group as a foreign key to store the team's name
 position: which states the user's rank in the hierachy of DM members 
 	(Dancer, Captain, Core, Exec)
@@ -17,21 +17,12 @@ class DanceUser(User):
 	position = models.CharField(max_length=15, default='Dancer')
 	events = models.ManyToManyField(Event)
 	shift = models.IntegerField(blank = True, default = 0)
-"""
-DanceStaff is the class that contains users that contain some level of authority in the
-hierarchy of DanceMarathon. In other words, they're the Captains, Core, and Exec members
-of DanceMarathon. 
 
-Data members:
-job_title: the position title given to the idividual.
-points: the number of points that person has earned.
-mon_raised: the amount of money the person has earned.
-"""
-class DanceStaff(DanceUser):
-	job_title = models.CharField(max_length=50,default='')
-	points = models.IntegerField(blank=True,default=0)
-	mon_raised = models.IntegerField(blank=True,default=0)
+	def __str__(self):
+		return (first_name + " " + last_name)
 
-class Dancer(DanceUser):
-	points = models.IntegerField(blank=True,default=0)
-	mon_raised = models.IntegerField(blank=True,default=0)
+	def get_position(self):
+		return self.position
+
+	def get_shift(self):
+		return self.shift
