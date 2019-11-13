c3f01f783bfff7edc199e1599bc5c8b80f9a3bbe
NickWattsCS <nsw12b@my.fsu.edu>
2019-11-13 15:52:50 -0500

    Removed serializers for Dancer and DanceStaff
    No longer necessary

--- users/serializers.py
+++ users/serializers.py
@@ -1,26 +1,8 @@
 from rest_framework import serializers
-from users.models import DanceUser,Dancer,DanceStaff,DanceGuest
+from .models import DanceUser
 
 class DanceUserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = DanceUser
 		fields = ["id", "first_name", "last_name", "email", "date_joined", "last_login", "organization", "mon_raised"]
 
-class DancerSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Dancer
-		fields = ["id", "first_name", "last_name", "email", "date_joined", "last_login", "organization", "mon_raised"]
-
-class DanceStaffSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = DanceStaff
-		fields = ["id", "first_name", "last_name", "email", "date_joined", "last_login", "organization", "mon_raised"]
-"""
-
-I will work on this model after I get the others to work
-
-class DanceGuestSerializer(serializers.ModelSerializers):
-	class Meta:
-		model = DanceGuest
-		fields = ["date_joined"]
-"""
