8f2d9f11b3c9524edd4aad4e24679fb8a3706b19
NickWattsCS <nsw12b@my.fsu.edu>
2019-11-13 15:58:01 -0500

    Removed viewsets for obsolete classes

--- users/views.py
+++ users/views.py
@@ -3,22 +3,7 @@ from users.models import *
 from users.serializers import *
 # Create your views here
 
-class UserViewSet(viewsets.ModelViewSet):
+class DanceUserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all().order_by("last_name")
 	serializer_class = UserSerializer
 
-class DancerViewSet(viewsets.ModelViewSet):
-	queryset = Dancer.objects.all().order_by("last_name")
-	serializer_class = DancerSerializer
-
-class StaffViewSet(viewsets.ModelViewSet):
-	queryset = Staff.objects.all().order_by("last_name")
-	serializer_class = StaffSerializer
-
-class AdminViewSet(viewsets.ModelViewSet):
-	queryset = Admin.objects.all().order_by("last_name")
-	serializer_class = AdminSerializer
-
-class GuestViewSet(viewsets.ModelViewSet):
-	queryset = Guest.objects.all().order_by("joined")
-	serializer_class = GuestSerializer
