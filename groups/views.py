296afb2b55fb1033ea9393f8a1482951ad4ef371
NickWattsCS <nsw12b@my.fsu.edu>
2019-11-15 18:19:23 -0500

    Minor changes to make migrations

--- groups/views.py
+++ groups/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import DanceGroup
 from .serializers import GroupSerializer
 
-class GroupViewSet(generics.ModelViewSet):
+class GroupViewSet(viewsets.ModelViewSet):
     queryset = AdminGroup.objects.all().order_by("name")
     serializer_class = GroupSerializer
