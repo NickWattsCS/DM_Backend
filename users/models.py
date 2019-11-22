620ede41538686cf68cecdf9b178021e2e685888
NickWattsCS <nsw12b@my.fsu.edu>
2019-11-21 22:40:18 -0500

    Added support for teams in users module

--- users/models.py
+++ users/models.py
@@ -10,11 +10,25 @@ from groups.models import DanceGroup
 DanceUser is the class from which its children inherit the data members:
 team: which uses Group as a foreign key to store the team's name
 position: which states the user's rank in the hierachy of DM members 
-	(Dancer, Captain, Core, Exec)
+	(Dancer, Captain, Coordinator, Exec)
 events: contains all events the user has signed up for
+shift: contains the day in which the user will attend Dance Marathon
+	(0 = no days, 1 = first shift, 2 = second shift, 3 = both shifts)
+team: contains a foreign key to the Team model, which contains the name of the child of
+	their team as well as the money they have raised
+internal_team: the department in which the user belongs if they're not a Dancer.
 """
 class DanceUser(AbstractUser):
-	#team = models.ForeignKey(DanceGroup, related_name="group", blank=True, on_delete=models.CASCADE)
-	position = models.CharField(max_length=15, default='Dancer')
-	events = models.ManyToManyField(Event, related_name="user_list", blank=True)
-	shift = models.IntegerField(blank = True, default = 0)
+	organization = models.ForeignKey(DanceGroup, related_name="group", null = True, on_delete=models.CASCADE)
+	position = models.CharField(max_length=15, null = True)
+	events = models.ManyToManyField(Event, related_name="user_list", null = True)
+	points = models.IntegerField(default = 0)
+	shift = models.IntegerField(null = True)
+	team = models.ForeignKey(Team, related_name="team", null = True, on_delete = models.CASCADE)
+	internal_team = models.CharField(max_length = 30, null = True)
+"""
+	def addInternalTeam(self, i_team)
+		if self.position != "Dancer":
+			self.internal_team = i_team
+
+"""
