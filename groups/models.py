2e6326c83de2b6a6f6743e667b267169af009278
ces17g <54723379+ces17g@users.noreply.github.com>
2019-11-26 09:22:35 -0500

    Added foreign key to groups
    Foreign key to Teams added

--- groups/models.py
+++ groups/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 """
-Organization name and total points
+Organization name, total funds raised, and total points
 """
 class DanceGroup(models.Model):
     name = models.CharField(max_length = 100)
     mon_raised = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
+    team = models.ForeignKey(Team, related_name="team", null = True, on_delete = models.CASCADE)
 
     class Meta:
         ordering = ('name',)
