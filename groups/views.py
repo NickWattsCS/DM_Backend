4da4dd54b92330a3e5a4ca1d1a1676864761146c
ces17g <54723379+ces17g@users.noreply.github.com>
2019-12-05 22:14:56 -0500

    Update DanceGroups views.py for excel export

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.http import HttpResponse
 from rest_framework import viewsets
 from .models import DanceGroup
 from .serializers import GroupSerializer
@@ -5,3 +8,16 @@ from .serializers import GroupSerializer
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = DanceGroup.objects.all()
     serializer_class = GroupSerializer
+
+def export_DanceGroups_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="DanceGroups.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Group', 'Funds Raised', 'Points'])
+
+    DanceGroups = DanceGroup.objects.all().values_list('name', 'mon_raised', 'points')
+    for DanceGroup in DanceGroups:
+        writer.writerow(DanceGroup)
+
+    return response
